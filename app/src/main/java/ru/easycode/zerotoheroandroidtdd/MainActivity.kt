c81ccece6c82df9ded940b5965090c6adf5fbfcf
mansu <mansurv@mail.ru>
2023-11-10 23:16:46 +0300

    task /022 solved

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -1,11 +1,52 @@
 package ru.easycode.zerotoheroandroidtdd
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import android.widget.TextView
+import androidx.core.view.children
+import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.actionButton.setOnClickListener {
+            val inputText = binding.inputEditText.text
+            addTextView(inputText.toString())
+            binding.inputEditText.setText("")
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val textList =
+            binding.contentLayout.children.map { (it as TextView).text.toString() }.toList()
+        outState.putStringArrayList(KEY, ArrayList(textList))
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val list = savedInstanceState.getStringArrayList(KEY)?: ArrayList()
+        list.forEach {inputText ->
+            addTextView(inputText)
+        }
+    }
+
+    fun addTextView(inputText: CharSequence) {
+        val textView = TextView(this).apply {
+            text = inputText
+            setTextColor(Color.RED)
+        }
+        binding.contentLayout.addView(textView)
+    }
+
+    companion object {
+        private const val KEY = "list"
     }
 }
