74fc67d912589e05ee9f1b0d619d4f73e303a537
mansu <mansurv@mail.ru>
2023-11-13 16:44:24 +0300

    task /024 with errors

--- app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
+++ app/src/main/java/ru/easycode/zerotoheroandroidtdd/MainActivity.kt
@@ -7,10 +7,35 @@ import ru.easycode.zerotoheroandroidtdd.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
+    private val adapter = Adapter()
+    private lateinit var viewModel: MainViewModel
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.recyclerView.adapter = Adapter()
+
+        viewModel = (application as App).viewModel
+
+        binding.actionButton.setOnClickListener {
+            val text = binding.inputEditText.text.toString()
+            viewModel.add(text)//myItemsAdapter.add(text)
+            binding.inputEditText.text?.clear()
+        }
+
+        viewModel.liveData().observe(this) {
+            adapter.update(it)
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        viewModel.save(BundleWrapper.Base(outState))//myItemsAdapter.save(outState)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        viewModel.restore(BundleWrapper.Base(savedInstanceState))//myItemsAdapter.restore(savedInstanceState)
     }
 }
